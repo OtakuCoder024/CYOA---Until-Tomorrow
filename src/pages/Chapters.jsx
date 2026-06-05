745ee7a20ad2f503c9f187bb19c33a2d9ebff553
OtakuCoder024 <anchetalancerhenier024@gmail.com>
2026-06-05 20:04:39 +0800

    something

--- src/pages/Chapters.jsx
+++ src/pages/Chapters.jsx
@@ -1,5 +1,28 @@
+import { FaBars, FaHeart, FaCog, FaBook } from "react-icons/fa";
+import '../styles/Chapters.css';
 function Chapters(){
 
+    return(
+        <>
+            <nav className="navbar">
+                <div className="nav-left">
+                    <FaBars />
+                </div>
+                <div className="nav-center">
+                    Chapter 1
+                </div>
+                <div className="nav-right">
+                    <FaHeart /> 
+                    <FaBook /> 
+                    <FaCog /> 
+                </div>
+            </nav>
+
+            <div className="chapter-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem a dicta dolorum eveniet, exercitationem esse, architecto eligendi magnam reiciendis facilis mollitia odit laborum tempore. Repellendus nihil non animi quia debitis.
+            </div>
+        </>
+    )
 
 }
 
