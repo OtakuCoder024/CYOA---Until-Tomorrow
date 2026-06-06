40e4c0738fd0e381f2519bd0e5a2440492956ff9
OtakuCoder024 <anchetalancerhenier024@gmail.com>
2026-06-06 18:52:00 +0800

    Menu Design

--- src/pages/Chapters.jsx
+++ src/pages/Chapters.jsx
@@ -1,25 +1,83 @@
-import { FaBars, FaHeart, FaCog, FaBook } from "react-icons/fa";
+import { useState } from "react";
+import { FaBars, FaHeart, FaCog, FaBook, FaSearch, FaDoorClosed, FaBoxOpen, FaPlay, FaSave, FaFile, FaHome, FaQuestion,  } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+
+import { storyNodes } from "../utils/StoryNodes.jsx";
 import '../styles/Chapters.css';
 function Chapters(){
+
+    const [currentNodeId, setCurrentNodeId] = useState("chapter_1");
+    const [menuIsVisible, setMenuIsVisible] = useState(false);
+    const currentNode = storyNodes[currentNodeId];
+
+    function Menu(){
+        setMenuIsVisible(prev => !prev)
+    }
 
     return(
         <>
             <nav className="navbar">
                 <div className="nav-left">
-                    <FaBars />
+                    <FaBars onClick={() => Menu()} />
+                        <div className={`menuContent ${menuIsVisible ? "open" : "closed"}`}>
+                            <span>Survivor Menu</span>
+                            <button className="resume" onClick={() => Menu()}><FaPlay />Resume</button>
+                            <button><FaSave />Save Progress</button>
+                            <button><FaFile />Load Progress</button>
+                            <button><FaHome />Main Menu</button>
+                            <button><IoMdClose />Exit</button>
+                        </div>
+                    <span>CHAPTER {currentNode.metadata.chapter}</span>
                 </div>
                 <div className="nav-center">
-                    Chapter 1
+                     DAY {currentNode.metadata.day}
                 </div>
                 <div className="nav-right">
                     <FaHeart /> 
+                        <div className="statusPage">
+                            <span className="statusTitle"></span>
+                        </div>
+
                     <FaBook /> 
-                    <FaCog /> 
+                        <div className="journalPage">
+
+                        </div>
+
+                    <FaBoxOpen />
+                        <div className="suppliesPage">
+
+                        </div>
+
+                    <FaCog />
+                        <div className="settingsPage">
+                            
+                        </div> 
                 </div>
             </nav>
 
             <div className="chapter-body">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem a dicta dolorum eveniet, exercitationem esse, architecto eligendi magnam reiciendis facilis mollitia odit laborum tempore. Repellendus nihil non animi quia debitis.
+                <div className="chapter-text">
+                    {currentNode.content.map((item, index) => (
+                        <p key={index}>{item.text}</p>
+                    ))}
+                </div>
+
+            <div className="pages-divider"><span>◆</span></div>
+                <div className="choices">
+                    {
+                        currentNode.choices.map((choice) => (
+                            <button className="choice"
+                            key={choice.id}
+                            onClick={() => setCurrentNodeId(choice.next)}>
+                                <div className="choice-left">
+                                    <FaSearch className="icon"/>
+                                    <span>{choice.text}</span>
+                                </div>
+                                <span className="choice-arrow">&gt;</span>
+                            </button>
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
---
+++ src/utils/StoryNodes.jsx
@@ -0,0 +1,11 @@
+import chapter1 from "../nodes/chapter1.json"
+import chapter1Video from "../nodes/chapter1Video.json"
+import chapter1Project from "../nodes/chapter1Project.json"
+import chapter1Breakfast from "../nodes/chapter1Breakfast.json"
+
+export const storyNodes = {
+    chapter_1: chapter1,
+    chapter_1_video: chapter1Video,
+    chapter_1_project: chapter1Project,
+    chapter_1_breakfast: chapter1Breakfast
+};
